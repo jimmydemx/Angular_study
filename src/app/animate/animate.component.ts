afe0fce4a8573e9024ada230605b98b49c8486ae
jamtesdemx <quan.wu@imaginanet.com>
2022-05-13 11:56:13 +0200

    animation

--- src/app/animate/animate.component.ts
+++ src/app/animate/animate.component.ts
@@ -1,15 +1,30 @@
+import { animate, animation, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-animate',
   templateUrl: './animate.component.html',
-  styleUrls: ['./animate.component.scss']
+  styleUrls: ['./animate.component.scss'],
+  animations:[
+    trigger('square', [state('state1',style({'background-color':'red'})),
+    state('state2',style({'background-color':'blue'})),
+    transition('state2 => state1',animate(1000))
+    ])
+  ]
 })
 export class AnimateComponent implements OnInit {
 
+  public squreState!:any;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  onClick(){
+
+    this.squreState = this.squreState== 'state1' ? 'state2': 'state1';
+
   }
 
 }
--- src/app/util/util.module.ts
+++ src/app/util/util.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AjvschemaComponent } from './ajvschema/ajvschema.component';
 import { UtilComponent } from './util.component';
 import { AceEditorModule } from 'ng2-ace-editor';
-import { AceEditorComponent } from './ace-editor/ace-editor.component';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SchemadommarkerDirective } from './ajvschema/schemadommarker.directive';
 import { CompComponent } from './direcCom/comp.component';
 import { DirecDirective } from './direcCom/direc.directive';
+import { CustomValueAccessComponent } from './custom-value-access/custom-value-access.component';
+import { CustomWrapperComponent } from './custom-value-access/custom-wrapper.component';
+import { CustomBridgeAComponent } from './custom-value-access/custom-bridge-a.component';
+import { CustomBridgeBComponent } from './custom-value-access/custom-bridge-b.component';
+
 
 
 
@@ -15,10 +19,15 @@ import { DirecDirective } from './direcCom/direc.directive';
   declarations: [
     AjvschemaComponent,
     UtilComponent,
-    AceEditorComponent,
     SchemadommarkerDirective,
     CompComponent,
     DirecDirective,
+    CustomValueAccessComponent,
+    CustomWrapperComponent,
+    CustomBridgeAComponent,
+    CustomBridgeBComponent,
+
+
     
     
   ],
